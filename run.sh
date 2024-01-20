7396d808561e430ec5096400a22713c3ed6060ac
Nikita Chervov <gcnikitachervov@gmail.com>
2024-01-20 22:24:50 +0300

    added conv filters, rewrote on pl and tensorboard

---
+++ run.sh
@@ -0,0 +1,29 @@
+# 1 conv filter
+python3 mnist_transformer/TDA_transformer.py data/mnist/1_conv_filter_train.pt data/mnist/1_conv_filter_test.pt transformer_1_conv_filter_run1
+python3 mnist_transformer/TDA_transformer.py data/mnist/1_conv_filter_train.pt data/mnist/1_conv_filter_test.pt transformer_1_conv_filter_run2
+python3 mnist_transformer/TDA_transformer.py data/mnist/1_conv_filter_train.pt data/mnist/1_conv_filter_test.pt transformer_1_conv_filter_run3
+
+# 4 conv filter
+python3 mnist_transformer/TDA_transformer.py data/mnist/4_conv_filter_train.pt data/mnist/4_conv_filter_test.pt transformer_4_conv_filter_run1
+python3 mnist_transformer/TDA_transformer.py data/mnist/4_conv_filter_train.pt data/mnist/4_conv_filter_test.pt transformer_4_conv_filter_run2
+python3 mnist_transformer/TDA_transformer.py data/mnist/4_conv_filter_train.pt data/mnist/4_conv_filter_test.pt transformer_4_conv_filter_run3
+
+# 10 conv filter
+python3 mnist_transformer/TDA_transformer.py data/mnist/10_conv_filter_train.pt data/mnist/10_conv_filter_test.pt transformer_10_conv_filter_run1
+python3 mnist_transformer/TDA_transformer.py data/mnist/10_conv_filter_train.pt data/mnist/10_conv_filter_test.pt transformer_10_conv_filter_run2
+python3 mnist_transformer/TDA_transformer.py data/mnist/10_conv_filter_train.pt data/mnist/10_conv_filter_test.pt transformer_10_conv_filter_run3
+
+# 1 dir filter
+python3 mnist_transformer/TDA_transformer.py data/mnist/1_dir_filter_train.pt data/mnist/1_dir_filter_test.pt transformer_1_dir_filter_run1
+python3 mnist_transformer/TDA_transformer.py data/mnist/1_dir_filter_train.pt data/mnist/1_dir_filter_test.pt transformer_1_dir_filter_run2
+python3 mnist_transformer/TDA_transformer.py data/mnist/1_dir_filter_train.pt data/mnist/1_dir_filter_test.pt transformer_1_dir_filter_run3
+
+# 4 dir filter
+python3 mnist_transformer/TDA_transformer.py data/mnist/4_dir_filter_train.pt data/mnist/4_dir_filter_test.pt transformer_4_dir_filter_run1
+python3 mnist_transformer/TDA_transformer.py data/mnist/4_dir_filter_train.pt data/mnist/4_dir_filter_test.pt transformer_4_dir_filter_run2
+python3 mnist_transformer/TDA_transformer.py data/mnist/4_dir_filter_train.pt data/mnist/4_dir_filter_test.pt transformer_4_dir_filter_run3
+
+# 10 dir filter
+python3 mnist_transformer/TDA_transformer.py data/mnist/10_dir_filter_train.pt data/mnist/10_dir_filter_test.pt transformer_10_dir_filter_run1
+python3 mnist_transformer/TDA_transformer.py data/mnist/10_dir_filter_train.pt data/mnist/10_dir_filter_test.pt transformer_10_dir_filter_run2
+python3 mnist_transformer/TDA_transformer.py data/mnist/10_dir_filter_train.pt data/mnist/10_dir_filter_test.pt transformer_10_dir_filter_run3
