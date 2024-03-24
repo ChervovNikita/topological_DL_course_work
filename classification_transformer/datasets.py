726904374f9ffe2056ea893cb86aa74767b12d28
Nikita Chervov <gcnikitachervov@gmail.com>
2024-03-24 19:33:54 +0300

    differentiable calculation of conv filtraion

--- classification_transformer/datasets.py
+++ classification_transformer/datasets.py
@@ -57,3 +57,15 @@ class MyDatasetCEDT(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         return self.X[idx], self.y[idx]
+
+
+class MNIST_Dataset(Dataset):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+        
+    def __len__(self):
+        return len(self.X)
+    
+    def __getitem__(self, idx):
+        return self.X[idx].reshape(28, 28), self.y[idx]
