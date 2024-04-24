47d6c3fd54399238bffbe6ccc65ab4d56181b47c
Nikita Chervov <gcnikitachervov@gmail.com>
2024-04-24 17:14:58 +0300

    added configure sh

--- configure.sh
+++ configure.sh
@@ -1,8 +1,22 @@
-# gpu for torch (for my local machine)
-# curl -O https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh
-# sh Miniconda3-latest-MacOSX-arm64.sh
+sudo apt update
+sudo apt install python3-pip
+pip3 install torch
+pip3 install loguru
+pip3 install pytorch_lightning
+pip3 install lightning
+pip3 install torchvision
+pip3 install gudhi
+pip3 install scipy
+pip3 install scikit-learn
+pip3 install matplotlib
+pip3 install ripser
+pip3 install diagram2vec
+pip3 install jupyterlab
+pip3 install -U 'tensorboardX'
+pip3 install giotto-tda
+pip3 install pandas 
 
-# pip3 install --pre torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/nightly/cpu
-
-# libs
-pip3 install -r requirements.txt
+cd experiments
+python3 get_datasets_script.py
+cd ..
+python3 experiments/training.py data/mnist/images_train.pt data/mnist/images_test.pt trainable_conv_10 conv 16 300 '{"n_dims": [1, 10], "n_hidden": 64, "n_out": 10, "nhead": 2, "num_layers": 2, "dim_feedforward": 256, "device": "cpu"}'
